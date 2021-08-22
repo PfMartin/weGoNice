3b63dbcdda5a81835abe385ced122606fc0f4a26
Martin <martinpfatrisch@gmail.com>
2021-08-22 14:06:11 +0200

    Added a color module

---
+++ frontend/src/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  accent: '#0066cc',
+  lightAccent: '#d0b691',
+  lightGrey: '#e8e7ed',
+  midGrey: '#8e8990',
+  darkGrey: '#34323d',
+  white: '#fff',
+};
+
+export default colors;
