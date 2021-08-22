a2e4932ffc750d28e8ccac5446d1770f48573ef5
Martin <martinpfatrisch@gmail.com>
2021-08-22 14:06:50 +0200

    Added the App component with the function to change between apps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'src/components/Structure/Navbar.js';
+import RecipesOverview from 'src/components/Recipes/RecipesOverview.js';
+import ReferencesOverview from 'src/components/References/ReferencesOverview.js';
 import Footer from 'src/components/Structure/Footer.js';
 
 const App = () => {
+  const [app, setApp] = useState('recipes');
+
+  /**
+   * Changes the app on click according to the targetapp attribute of the clicked element. The clicked element is usually an Icon. So you may look into 'src/components/Structure/IconFrame.js'
+   * @param  {[type]} e [description]
+   * @return {[type]}   [description]
+   */
+  const onChangeApp = (e) => {
+    const targetApp = e.currentTarget.getAttribute('targetapp');
+    setApp(targetApp);
+  };
+
   return (
     <div className="app">
-      <Navbar />
+      <Navbar onChangeApp={onChangeApp} app={app} />
+      {app === 'recipes' ? <RecipesOverview /> : <ReferencesOverview />}
       <Footer />
     </div>
   );
