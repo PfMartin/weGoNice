80bce0090de9d31c41ff5db52a4ef5e51e21d605
Martin <martinpfatrisch@gmail.com>
2021-08-24 06:43:36 +0200

    Restructured the options bar for recipes

--- frontend/src/components/Recipes/RecipesOverview.js
+++ frontend/src/components/Recipes/RecipesOverview.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import SiteHeader from 'src/components/Structure/SiteHeader.js';
 import IconFrame from 'src/components/Structure/IconFrame.js';
 import FilterTag from 'src/components/Structure/FilterTag.js';
-import FilterOptions from 'src/components/Recipes/FilterOptions.js';
 import { BiFilterAlt, BiSearch } from 'react-icons/bi';
 
 const RecipesOverview = (props) => {
@@ -14,7 +13,6 @@ const RecipesOverview = (props) => {
     dessert: true,
     drinks: true,
   });
-  const [filterOptions, setFilterOptions] = useState(true);
 
   /**
    * Removes the cicked filter from the filter tags section
@@ -28,58 +26,49 @@ const RecipesOverview = (props) => {
     });
   };
 
-  const onToggleFilterOptions = () => {
-    const prevFilterOptions = filterOptions;
-    setFilterOptions(!prevFilterOptions);
-  };
-
   return (
     <div className="recipes-overview">
       <SiteHeader headline="Recipes" />
-      <div className="filter-section">
-        <div className="filter-headline">
+      <div className="recipe-options">
+        <div className="filter-section">
           <h3>Filter</h3>
-          <IconFrame onClick={onToggleFilterOptions}>
-            <BiFilterAlt />
-          </IconFrame>
-          {filterOptions ? <FilterOptions /> : ''}
-        </div>
-        <div className="filter-tags">
-          {filter.basics ? (
-            <FilterTag name="basics" onRemoveFilter={onRemoveFilter}>
-              basics
-            </FilterTag>
-          ) : (
-            ''
-          )}
-          {filter.breakfast ? (
-            <FilterTag name="breakfast" onRemoveFilter={onRemoveFilter}>
-              breakfast
-            </FilterTag>
-          ) : (
-            ''
-          )}
-          {filter.main ? (
-            <FilterTag name="main" onRemoveFilter={onRemoveFilter}>
-              main
-            </FilterTag>
-          ) : (
-            ''
-          )}
-          {filter.dessert ? (
-            <FilterTag name="dessert" onRemoveFilter={onRemoveFilter}>
-              dessert
-            </FilterTag>
-          ) : (
-            ''
-          )}
-          {filter.drinks ? (
-            <FilterTag name="drinks" onRemoveFilter={onRemoveFilter}>
-              drinks
-            </FilterTag>
-          ) : (
-            ''
-          )}
+          <div className="filter-tags">
+            {filter.basics ? (
+              <FilterTag name="basics" onRemoveFilter={onRemoveFilter}>
+                basics
+              </FilterTag>
+            ) : (
+              ''
+            )}
+            {filter.breakfast ? (
+              <FilterTag name="breakfast" onRemoveFilter={onRemoveFilter}>
+                breakfast
+              </FilterTag>
+            ) : (
+              ''
+            )}
+            {filter.main ? (
+              <FilterTag name="main" onRemoveFilter={onRemoveFilter}>
+                main
+              </FilterTag>
+            ) : (
+              ''
+            )}
+            {filter.dessert ? (
+              <FilterTag name="dessert" onRemoveFilter={onRemoveFilter}>
+                dessert
+              </FilterTag>
+            ) : (
+              ''
+            )}
+            {filter.drinks ? (
+              <FilterTag name="drinks" onRemoveFilter={onRemoveFilter}>
+                drinks
+              </FilterTag>
+            ) : (
+              ''
+            )}
+          </div>
         </div>
         <form className="search-section">
           <input placeholder="Search Recipes"></input>
