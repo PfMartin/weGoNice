5028b328fe7d28990ba9fdde290f3483e7b6f7dd
Martin <martinpfatrisch@gmail.com>
2021-11-02 14:29:01 +0100

    Adjusted the database volume to the server directory

--- deployment/run_api.sh
+++ deployment/run_api.sh
@@ -2,6 +2,7 @@
 
 podport="8000"
 database_env="/home/server/weGoNice/source/backend/Database.env"
+database_volume="/home/server/weGoNice/weGoNice-database"
 
 echo "Which version of the app do you want to run?"
 read version
@@ -22,7 +23,7 @@ podman run \
   --detach \
   --rm \
   --env-file ${database_env} \
-  --volume /home/ubuntu/weGoNice-database:/var/lib/postgresql/data \
+  --volume ${database_volume}:/var/lib/postgresql/data \
   --pod weGoNice-api-pod \
   docker.io/library/postgres:13.4
 
