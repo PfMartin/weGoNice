00c84d1bfce6a57561f9409be83f40c6ac0e2bbf
Martin <martinpfatrisch@gmail.com>
2021-11-08 17:51:31 +0100

    Started creating the SectionForm component

---
+++ source/frontend/src/components/Recipes/SectionForm/SectionForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionForm = () => {
+  return (
+    <div className="section-form">
+      <h1>SectionForm</h1>
+    </div>
+  );
+};
+
+export default SectionForm;
