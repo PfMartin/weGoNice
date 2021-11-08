39685ce9100f2c5069e49ce1dfebcd719f2a96d1
Martin <martinpfatrisch@gmail.com>
2021-11-08 10:21:39 +0100

    Declared different url for development and production

--- source/frontend/src/apis/weGoNice.js
+++ source/frontend/src/apis/weGoNice.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+const PORT = 8000;
+const url =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:${PORT}`
+    : `http://192.168.178.44:${PORT}`;
+
 export default axios.create({
-  baseURL: 'http://192.168.178.44:8000',
+  baseURL: url,
 });
