b2bd8b02ad3e67f5a4e922001fa187f98dd5bdf5
Martin <martinpfatrisch@gmail.com>
2022-06-21 15:03:15 +0200

    Fixed test for empty users array and user creation

--- source/server/users/users_test.go
+++ source/server/users/users_test.go
@@ -3,39 +3,24 @@ package users
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-const url = "http://localhost:8080/users"
+const url = "/users"
 
-// func TestUserCreation(t *testing.T) {
-// 	resp, err := http.Get(url)
+var testUser = map[string]interface{}{
+	"id":        "1",
+	"lastname":  "Puh",
+	"firstname": "Kak",
+	"age":       float64(18), // Needs to be float64 (json.Unmarshal converts any number to float64)
+	"email":     "k@te.st",
+}
 
-// 	if err != nil {
-// 		t.Errorf("Get request to %s failed: %s", url, err)
-// 	}
-
-// 	defer resp.Body.Close()
-// 	body, err := ioutil.ReadAll(resp.Body)
-
-// 	if err != nil {
-// 		t.Errorf("Reading boyd failed: %s", err)
-// 	}
-
-// 	bodyString := string(body)
-
-// 	expected := `[]`
-// 	if bodyString != expected {
-// 		t.Errorf("handler returned unexpected body: got %v want %v", bodyString, expected)
-// 	}
-// }
-
-// TEST DOESN'T WORK SO FAR
 func TestEmptyTable(t *testing.T) {
-	req, err := http.NewRequest("GET", "/users", nil)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -48,29 +33,21 @@ func TestEmptyTable(t *testing.T) {
 		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	if body := rr.Body.String(); body != "[]" {
-		t.Errorf("Expected an empty array. Got %s", body)
+	want := "[]"
+
+	if body := rr.Body.String(); strings.Trim(body, "\n") != want {
+		t.Errorf("Got: %s, Want: %s", body, want)
 	}
 }
 
 func TestCreateUser(t *testing.T) {
+	payload, err := json.Marshal(testUser)
 
-	newUser := map[string]interface{}{
-		"id":        "1",
-		"lastname":  "Puh",
-		"firstname": "Kak",
-		"age":       18,
-		"email":     "k@te.st",
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	// TODO: Create payload from newUser map
-	fmt.Println(newUser)
-
-	// payloadString := `"id":` + newUser[|]
-
-	payload := []byte(`{"id": "1","lastname":"Puh","firstname":"Kak","age": 18,"email":"k@te.st"}`)
-
-	req, err := http.NewRequest("POST", "/users", bytes.NewBuffer(payload))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -86,23 +63,10 @@ func TestCreateUser(t *testing.T) {
 	var m map[string]interface{}
 	json.Unmarshal(rr.Body.Bytes(), &m)
 
-	if m["id"] != "1" {
-		t.Errorf("Expected user id to be '1'. Got '%v'", m["id"])
-	}
+	for key, val := range testUser {
 
-	if m["lastname"] != "Puh" {
-		t.Errorf("Expected user lastname to be 'Puh'. Got '%v'", m["lastname"])
-	}
-
-	if m["firstname"] != "Kak" {
-		t.Errorf("Expected user firstname to be 'Kak'. Got '%v'", m["firstname"])
-	}
-
-	if m["age"] != 18.0 {
-		t.Errorf("Expected user age to be '18'. Got '%v'", m["age"])
-	}
-
-	if m["email"] != "k@te.st" {
-		t.Errorf("Expected user email to be 'k@te.st'. Got '%v'", m["email"])
+		if m[key] != val {
+			t.Errorf("Expected user %s to be '%v'. Got '%v' instead. %T, %T", key, val, m[key], val, m[key])
+		}
 	}
 }
