ea2e5a17b88d73e9d5c3ef358932d863d0c75749
Martin <martinpfatrisch@gmail.com>
2022-09-27 22:02:06 +0200

    Started with the login view

--- source/frontend/src/App.vue
+++ source/frontend/src/App.vue
@@ -9,10 +9,12 @@
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
 import { useStore } from 'vuex';
+import authenticate from './auth';
 
 export default defineComponent({
   name: 'App',
   setup() {
+    authenticate();
     const store = useStore();
 
     const isAuthenticated = computed(() => store.getters.isAuthenticated);
@@ -25,12 +27,19 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&display=swap');
+@import './styles/colors.scss';
+
+body {
+  background: $main-bg-color;
+}
+
 #app {
-  padding: 1rem;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  padding: 0.5rem;
+  font-family: Monserrat, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  color: $text-color;
 }
 
 nav {
--- source/frontend/src/views/LoginView.vue
+++ source/frontend/src/views/LoginView.vue
@@ -1,18 +1,100 @@
 <template>
-  <div class="login-page">Login Page</div>
+  <div class="login-view">
+    <main class="main-content">
+      <h2>Login</h2>
+      <section class="card">
+        <form action="apply">
+          <div class="input">
+            <label for="email">Email</label>
+            <input id="email" v-model="email" />
+          </div>
+          <div class="input">
+            <label for="password">Password</label>
+            <input id="password" type="password" v-model="password" />
+          </div>
+        </form>
+      </section>
+      <footer>
+        <small>
+          Not registered yet? <router-link to="/about">Register</router-link>
+        </small>
+      </footer>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { useStore } from 'vuex';
 
 export default defineComponent({
   name: 'LoginView',
   setup() {
     const store = useStore();
-    console.log(store.getters.isAuthenticated);
+    console.log('LoginView: ' + store.getters.isAuthenticated);
+
+    const email = ref<string>('');
+    const password = ref<string>('password');
+
+    return {
+      email,
+      password,
+    };
   },
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '../styles/colors.scss';
+
+.login-view {
+  display: flex;
+  justify-content: center;
+  .main-content {
+    padding: 1rem;
+    background: #000;
+    border-radius: 5px;
+    width: 500px;
+
+    .card {
+      background: $card-bg-color;
+      padding: 1rem;
+      border-radius: 5px;
+
+      form {
+        display: flex;
+        flex-direction: column;
+
+        .input {
+          display: flex;
+          flex-direction: column;
+
+          input {
+            border-radius: 3px;
+            box-shadow: none;
+            border: none;
+            outline: 2px solid #333;
+            margin: 0.5rem 0;
+            padding: 0.5rem;
+
+            &:focus {
+              outline: 2px solid $accent-color;
+            }
+          }
+        }
+      }
+    }
+
+    footer {
+      padding: 0.5rem 0.5rem 0 0.3rem;
+      display: flex;
+      justify-content: flex-end;
+
+      a {
+        text-decoration: none;
+        color: $accent-color;
+      }
+    }
+  }
+}
+</style>
