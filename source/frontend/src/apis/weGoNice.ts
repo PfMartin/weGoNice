150d3661e2a62469c84bf227c19d7b15bfd21581
Martin <martinpfatrisch@gmail.com>
2022-10-03 13:30:17 +0200

    Added axios and created the post request

---
+++ source/frontend/src/apis/weGoNice.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const url = 'http://localhost:8000';
+const headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
+
+export const createUser = async (body: { email: string; password: string }) => {
+  try {
+    const { data } = await axios.post(`${url}/users`, body, {
+      headers,
+    });
+    return data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      console.log('Axios error: ', err.message);
+      return err.message;
+    } else {
+      console.log('Unexpected error: ', err);
+      return 'An unexpected error occured';
+    }
+  }
+};
