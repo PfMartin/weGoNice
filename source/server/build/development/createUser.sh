39798f9fc00d7320196c85703fa16272a9f80bcb
Martin <martinpfatrisch@gmail.com>
2022-10-31 13:23:56 +0100

    Refined script for test container

--- source/server/build/development/createUser.sh
+++ source/server/build/development/createUser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# https://gist.github.com/sarjarapu/761e4fc1ea7c5d46f1fb64d973d5a6bf
 docker exec weGoNice-test-db \
   mongosh weGoNice -u TestUser -p testPassword --authenticationDatabase admin \
   --eval "db.createUser({user: 'TestUser', pwd: 'testPassword', roles: [{role: 'readWrite', db: 'weGoNice'}]})"
