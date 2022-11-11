626f43f14b45af0aaedc6e241136fca064f61bcf
Martin <martinpfatrisch@gmail.com>
2022-11-11 06:59:14 +0100

    Started creating the Dropdown component

---
+++ source/frontend/src/components/DropdownInput.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="dropdown-input">
+    <label for="dropdown"
+      ><div class="label-text">
+        <ion-icon name="list" /> &nbsp; <span>Sort By</span>
+      </div>
+    </label>
+    <div id="dropdown" class="dropdown">
+      <div class="selected">
+        <span class="selected-item">None</span>
+        <ion-icon name="chevron-down" />
+      </div>
+      <ul class="dropdown-content">
+        <li>Option 1</li>
+        <li>Option 2</li>
+        <li>Option 3</li>
+        <li>Option 4</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import '../styles/outline.scss';
+@import '../styles/colors.scss';
+
+.dropdown-input {
+  label {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .label-text {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  .dropdown {
+    position: relative;
+    margin: 0.5rem 0;
+
+    .selected {
+      background: $bg-color-mid;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      color: $bg-color-light;
+      min-width: 300px;
+
+      padding: 0.5rem;
+      border-radius: $border-radius;
+
+      ion-icon {
+        color: $text-color;
+      }
+    }
+
+    ul.dropdown-content {
+      width: 100%;
+      position: absolute;
+      z-index: 5;
+      top: 20px;
+      list-style: none;
+      text-decoration: none;
+      padding: 0;
+      background: $bg-color-mid;
+      color: $text-color;
+      border-radius: $border-radius;
+
+      li {
+        padding: 0.5rem 1rem;
+        transition: all 0.1s;
+
+        &:hover {
+          background-color: $accent-color;
+          color: $bg-color-mid;
+          border-radius: $border-radius;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+}
+</style>
--- source/frontend/src/components/ListControl.vue
+++ source/frontend/src/components/ListControl.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="list-control">
-    <h1>List Control</h1>
     <div class="controls">
-      <p>Name Ascending Descending</p>
+      <div class="dropdown-container">
+        <DropdownInput />
+      </div>
+      <!-- <p>Name Ascending Descending</p>
       <p>Amount Recipes Ascending Descending</p>
       <p>Toggle show authors with 0 Recipes</p>
       <p>Creation Date Ascending Descending</p>
       <p>Modification Date Ascending Descending</p>
+      <p>Toggle for youtube, instagram, website</p> -->
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DropdownInput from '@/components/DropdownInput.vue';
+</script>
 
 <style scoped lang="scss">
 @import '../styles/colors.scss';
@@ -19,7 +24,7 @@
 
 .list-control {
   margin-left: $nav-bar-width;
-  padding: 1rem;
+  padding: 1rem 2rem;
 
   h1 {
     padding: 0;
