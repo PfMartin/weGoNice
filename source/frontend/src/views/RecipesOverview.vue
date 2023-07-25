544ec82713fe473df9fade6b2343818f93c7019f
Martin <martinpfatrisch@gmail.com>
2023-07-25 16:23:40 +0200

    Added OverviewControl and implemented it into recipes overview

--- source/frontend/src/views/RecipesOverview.vue
+++ source/frontend/src/views/RecipesOverview.vue
@@ -4,10 +4,11 @@ import { getAllRecipes } from '@/apis/weGoNice/recipes';
 import { RECIPE_SORTING_OPTIONS, SortDirections } from '@/utils/constants';
 import DropdownInput from '@/components/DropdownInput.vue';
 import RecipeCard from '@/components/RecipeCard.vue';
+import OverviewControl from '@/components/OverviewControl.vue';
 
-const selectedOption = ref('Name');
+const selectedSortingOption = ref('Name');
 const setSelectedOption = (option: string): void => {
-  selectedOption.value = option;
+  selectedSortingOption.value = option;
   sortRecipes();
 };
 
@@ -20,17 +21,13 @@ const toggleSortDirection = (): void => {
   sortRecipes();
 };
 
-const sortDirectionIcon = computed((): string =>
-  sortDirection.value === SortDirections.ASC ? 'arrow-down' : 'arrow-up'
-);
-
 const sortRecipes = (): void => {
   const sortKey: string =
-    selectedOption.value.charAt(0).toLowerCase() +
-    selectedOption.value.slice(1);
+    selectedSortingOption.value.charAt(0).toLowerCase() +
+    selectedSortingOption.value.slice(1);
 
   recipes.value = recipes.value.sort((a: any, b: any) => {
-    switch (selectedOption.value) {
+    switch (selectedSortingOption.value) {
       case 'Author':
         if (a.author.name && b.author.name) {
           if (a.author.name < b.author.name) {
@@ -89,26 +86,13 @@ onMounted(async () => {
 
 <template>
   <div class="recipes-overview">
-    <div class="list-control">
-      <div class="controls">
-        <div class="sorting">
-          <div class="dropdown-container">
-            <DropdownInput
-              :options="RECIPE_SORTING_OPTIONS"
-              :selectedOption="selectedOption"
-              @select-option="setSelectedOption"
-              id="recipeSortBy"
-              label="Sort By"
-              iconName="list"
-              width="300px"
-            />
-          </div>
-          <span @click="toggleSortDirection" class="sort-direction"
-            ><ion-icon :name="sortDirectionIcon"
-          /></span>
-        </div>
-      </div>
-    </div>
+    <OverviewControl
+      :sortingOptions="RECIPE_SORTING_OPTIONS"
+      :selectedOption="selectedSortingOption"
+      :sortingDirection="sortDirection"
+      @set-sorting-key="setSelectedOption"
+      @toggle-sorting-direction="toggleSortDirection"
+    />
 
     <div class="recipes" v-if="isReady" :style="`max-height: ${listHeight}px`">
       <template v-for="recipe in visibleRecipes" :key="recipe.name">
@@ -133,42 +117,6 @@ onMounted(async () => {
 .recipes-overview {
   margin-left: $nav-bar-width;
   padding: 1rem 1rem;
-
-  .list-control {
-    padding: 0 1rem;
-    h1 {
-      padding: 0;
-      margin: 0;
-    }
-
-    .controls {
-      display: flex;
-      align-items: center;
-      justify-content: flex-start;
-
-      .sorting {
-        display: flex;
-        margin-right: 1rem;
-        .sort-direction {
-          margin-left: 0.2rem;
-          font-size: 1.5rem;
-          color: $bg-color-mid;
-          display: flex;
-          align-items: center;
-
-          &:hover {
-            cursor: pointer;
-            color: $bg-color-dark;
-          }
-        }
-      }
-
-      .filter-switches {
-        display: flex;
-        gap: 0.5rem;
-      }
-    }
-  }
 
   .recipes {
     margin: 2rem 0;
