19731bb2cf72cc525176d7219538bedeb1bd8741
Martin <martinpfatrisch@gmail.com>
2023-08-05 21:06:21 +0200

    Added styling for scrollbar

--- source/frontend/src/App.vue
+++ source/frontend/src/App.vue
@@ -39,6 +39,7 @@ const notifications = computed(
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&display=swap');
 @import '@/styles/colors.scss';
 @import '@/styles/outline.scss';
+@import '@/styles/scroll-bar.scss';
 
 body {
   background: $bg-color-light;
