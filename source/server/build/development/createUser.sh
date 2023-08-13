7b76faad9c721bf32ef85c30c8ca8559abf681c7
Martin <martinpfatrisch@gmail.com>
2023-08-13 16:51:19 +0200

    Some clean up

--- source/server/build/development/createUser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# https://gist.github.com/sarjarapu/761e4fc1ea7c5d46f1fb64d973d5a6bf
-docker exec weGoNice-test-db \
-  mongosh weGoNice -u TestUser -p testPassword --authenticationDatabase admin \
-  --eval "db.createUser({user: 'TestUser', pwd: 'testPassword', roles: [{role: 'readWrite', db: 'weGoNice'}]})"
